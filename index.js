129dec64ad08a1e2ca8c6edcc33987ff23e92bc1
Bestun Gardi <bestun95@yahoo.com>
2018-08-05 22:33:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function overwriteBestCustomer(){
   bestCustomer = 'maybe bob';
 }
 
-let leastFavoriteCustomer = 4;
+const leastFavoriteCustomer = 4;
 
 function changeLeastFavoriteCustomer() {
   
